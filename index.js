839b54a43becf1a98a4fed8420b122060dafc465
Kyrune <jn-01@centurytel.net>
2020-08-27 10:31:01 -0500

    Add function shell

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Create a six-sided die
-function roll() {
-    console.log(Math.floor((Math.random() * 6) + 1));
+// function roll() {
+//     console.log(Math.floor((Math.random() * 6) + 1));
+// }
+
+function roll(min, max) {
+    
 }
 
 console.log(roll());
